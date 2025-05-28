02c6eeaff19f23d2de5081cdc4fe9b3d5fd98be4
Quốc Hùng <tqhung0105@gmail.com>
2025-05-28 20:27:18 +0700

    clean (#24)

--- build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hungqt/res/build/_deps/googletest-src/googlemock
-# Build directory: /home/hungqt/res/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hungqt/res/tests
-# Build directory: /home/hungqt/res/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
